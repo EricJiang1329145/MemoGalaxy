f68582a0609fa113ed9ccc72c2640de315457f95
EricJiang1329145 <jmr_eric@outlook.com>
2025-05-10 12:58:52 +0800

    Initial Commit

---
+++ MemoGalaxy/MemoGalaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGalaxyApp.swift
+//  MemoGalaxy
+//
+//  Created by Eric Jiang on 2025/5/10.
+//
+
+import SwiftUI
+
+@main
+struct MemoGalaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoGalaxyTests/MemoGalaxyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGalaxyTests.swift
+//  MemoGalaxyTests
+//
+//  Created by Eric Jiang on 2025/5/10.
+//
+
+import Testing
+@testable import MemoGalaxy
+
+struct MemoGalaxyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
